44305e5958f8c18ea2b0b3a5b2371dd06cb5e02b
Andy Aylward <aaylward@gmail.com>
2016-07-17 19:16:44 -0400

    add UnhandledExceptionMapper

---
+++ LunarCat/src/main/java/org/snackunderflow/providers/UnhandledExceptionMapper.java
@@ -0,0 +1,39 @@
+package org.snackunderflow.providers;
+
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+public class UnhandledExceptionMapper implements ExceptionMapper<Exception> {
+  @Override
+  public Response toResponse(Exception e) {
+    if (e instanceof NotFoundException) {
+      return Response.status(404).entity(new ErrorResponse("not found")).build();
+    }
+
+    if (e instanceof BadRequestException) {
+      return Response.status(400).entity(new ErrorResponse("bad request")).build();
+    }
+
+    return Response.serverError().entity(new ErrorResponse("internal error")).build();
+  }
+
+  private static class ErrorResponse {
+    private final String message;
+
+    private ErrorResponse(String message) {
+      this.message = message;
+    }
+
+    public String getMessage() {
+      return message;
+    }
+  }
+}
+
